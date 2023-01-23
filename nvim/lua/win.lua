9b8d3c9c6f666b18e3fa07e4125c34056b3cd73d
Serhii Bura <s.bura@pinglestudio.com>
2023-01-23 11:41:07 +0200

    Update windows terminal shell
    Signed-off-by: Serhii Bura <s.bura@pinglestudio.com>

--- nvim/lua/win.lua
+++ nvim/lua/win.lua
@@ -1,10 +1,9 @@
 vim.cmd [[
-    let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
-    let &shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
-    let &shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait'
-    let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-    set shellquote= shellxquote=
-]]
+        let &shell = has('win32') ? 'powershell' : 'pwsh'
+        let &shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
+        let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+        let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+        set shellquote= shellxquote=]]
 
 -- Open/Load config
 local keymap = vim.keymap
