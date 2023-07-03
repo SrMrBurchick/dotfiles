c7c9635d69113d116302d93f4de182e5c94357d2
Serhii Bura <srmrburchick@gmail.com>
2023-07-03 22:33:55 +0300

    nvim: Update nvim-tree git glyphs
    Signed-off-by: Serhii Bura <srmrburchick@gmail.com>

--- nvim/after/plugin/nvim-tree.rc.lua
+++ nvim/after/plugin/nvim-tree.rc.lua
@@ -8,7 +8,6 @@ end
 -- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
-
 nvim_tree.setup({
     sort_by = "case_sensitive",
     view = {
@@ -27,6 +26,29 @@ nvim_tree.setup({
         -- custom = { '^.git$' }
     },
     git = {
-        enable = false,
+        enable = true,
+    },
+    renderer = {
+        icons = {
+            glyphs = {
+                git = {
+                    unstaged = "",
+                    staged = "",
+                    unmerged = "󰿡",
+                    renamed = "󰛿",
+                    untracked = "",
+                    deleted = "",
+                    ignored = "󰮔",
+                },
+            },
+        },
     }
 })
+
+-- set highlights
+vim.o.termguicolors = true
+vim.cmd.highlight 'NvimTreeGitStaged guifg=green'
+vim.cmd.highlight 'NvimTreeGitDirty guifg=#e3b341'
+vim.cmd.highlight 'NvimTreeGitNew guifg=gray'
+vim.cmd.highlight 'NvimTreeGitRenamed guifg=#f0883e'
+vim.cmd.highlight 'NvimTreeGitDeleted guifg=red'
