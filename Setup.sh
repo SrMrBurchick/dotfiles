4c130afd4f26578b10c0dc4cb0ba826ca9e03095
SrMrBurchick <srmrburchick@gmail.com>
2025-07-12 22:24:03 +0300

    Update setup
    Signed-off-by: SrMrBurchick <srmrburchick@gmail.com>

--- Setup.sh
+++ Setup.sh
@@ -6,13 +6,6 @@ sudo pacman -S hyprland lightdm kitty fish waybar wofi firefox swww secrets naut
 echo "Install starship"
 curl -sS https://starship.rs/install.sh | sh
 
-# Bluetooth
-echo "Setup bluetooth"
-sudo pacman -S blueman bluez-utils
-
-sudo systemctl enable bluetooth.service
-sudo systemctl start bluetooth.service
-
 # Fonts
 echo "Setup fonts"
 sudo pacman -S ttf-3270-nerd ttf-hack ttf-font-awesome ttf-jetbrains-mono ttf-jetbrains-mono-nerd
@@ -28,13 +21,14 @@ git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
 # Link configs
 echo "Link configs"
-# ln -s "$PWD/hypr" "$HOME/.config/hypr"
+ln -s "$PWD/hypr" "$HOME/.config/hypr"
 # ln -s "$PWD/waybar" "$HOME/.config/waybar"
-# ln -s "$PWD/fish" "$HOME/.config/fish"
-# ln -s "$PWD/wofi" "$HOME/.config/wofi"
-# ln -s "$PWD/kitty" "$HOME/.config/kitty"
-# ln -s "$PWD/nvim" "$HOME/.config/nvim
+ln -s "$PWD/fish" "$HOME/.config/fish"
+ln -s "$PWD/wofi" "$HOME/.config/wofi"
+ln -s "$PWD/kitty" "$HOME/.config/kitty"
+ln -s "$PWD/nvim" "$HOME/.config/nvim"
 
 
 # Dark themes
+echo "Setup GTK theme"
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
